f39f8b848bb6b8b000168abc96010baf81bad4d3
Rocky Haque <rockyhaque99@gmail.com>
2024-07-11 23:15:12 +0600

    bid count added

--- index.js
+++ index.js
@@ -116,6 +116,14 @@ async function run() {
       }
 
       const result = await bidCollection.insertOne(bidData);
+
+      // update bid count in jobs collection
+      const updateDoc = {
+        $inc: {bid_count: 1}
+      }
+      const jobQuery = {_id: new ObjectId(bidData.jobId)}
+      const updateBidCount = await jobCollection.updateOne(jobQuery, updateDoc);
+
       res.send(result);
     });
 
